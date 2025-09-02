187891e55d823352b858f6b0a385222e4af5c09d
jadabrown822 <jadabrown822@gmail.com>
2025-09-02 11:33:44 -0500

    adding project 1

---
+++ Project 1 Recursion/hanoi.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <random>
+#include <cstdlib>
+using namespace std;
+
+
+int main() {
+    int n = 0;
+    int stepCount = 0;
+
+
+    for(int i=3; i<=35; i++){
+        n = i;
+    }
+}
