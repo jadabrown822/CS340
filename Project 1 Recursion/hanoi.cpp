310fac9fb01ea1ac5de62ab04bfb308831b72164
jadabrown822 <jadabrown822@gmail.com>
2025-09-03 13:40:33 -0500

    adiing int main function

--- Project 1 Recursion/hanoi.cpp
+++ Project 1 Recursion/hanoi.cpp
@@ -5,16 +5,29 @@
 using namespace std;
 
 
-
+int tower(int diskNum, char sourcePeg, char targetPeg, char tempPeg);
 
 int main() {
     int n = 0;
-    int stepCount = 0;
 
 
-    for(int i=3; i<=35; i++){
-        n = i;
+    for(int i=3; i<=35; ++i) {
+        int stepCount = 0;
 
+
+        auto start_time = chrono::high_resolution_clock::now();
+
+        stepCount = tower(i, 'A', 'C', 'B');
+
+        auto end_time = chrono::high_resolution_clock::now();
+
+        auto duration = end_time - start_time;
+
+        long long milliseconds = chrono::duration_cast<chrono::milliseconds>(duration).count();
+
+        cout << "N = " << n << endl;
+        cout << "That took " << stepCount << " steps." << endl;
+        cout << milliseconds << " milliseconds." << endl;
 
     }
 }
