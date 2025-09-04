4773c18881c3ac9434b8dd6615aa9d8ef384c241
jadabrown822 <jadabrown822@gmail.com>
2025-09-04 17:27:55 -0500

    adding power3.cpp

--- Project 1 Recursion/power3.cpp
+++ Project 1 Recursion/power3.cpp
@@ -7,6 +7,37 @@ using namespace std;
 
 int main() {
 
-    
+    for(int i=3; i<=35; ++i) {
+        auto start_time = chrono::high_resolution_clock::now();
+
+        // RM4(a,n)
+
+        auto end_time = chrono::high_resolution_clock::now();
+
+        auto duration = end_time - start_time;
+
+        long long nanoseconds = chrono::duration_cast<chrono::nanoseconds>(duration).count();
+
+        cout << "N = " << i << endl;
+        // cout << answer << endl;
+        cout << nanoseconds << " nanoseconds." << endl;
+    }
+
+    for(int i=3; i<=35; ++i) {
+        auto start_time = chrono::high_resolution_clock::now();
+
+        // RM6(a,n)
+
+        auto end_time = chrono::high_resolution_clock::now();
+
+        auto duration = end_time - start_time;
+
+        long long nanoseconds = chrono::duration_cast<chrono::nanoseconds>(duration).count();
+        
+        cout << "N = " << i << endl;
+        // cout << answer << endl;
+        cout << nanoseconds << " nanoseconds." << endl;
+    }
+
     return 0;
 }
