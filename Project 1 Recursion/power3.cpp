5c08c94aecba20241b2ba6897fb05048f9335fb9
jadabrown822 <jadabrown822@gmail.com>
2025-09-08 14:42:50 -0500

    it runs, but negative numebers

--- Project 1 Recursion/power3.cpp
+++ Project 1 Recursion/power3.cpp
@@ -5,15 +5,15 @@
 
 using namespace std;
 
-int RM4(int base, int power);
-int RM6(int base, int power);
+int RM4(int a, int n);
+int RM6(int a, int n);
 
 int main() {
 
     for(int i=3; i<=35; ++i) {
         auto start_time = chrono::high_resolution_clock::now();
 
-        // RM4(a,n)
+        long long answer = RM4(3,i);
 
         auto end_time = chrono::high_resolution_clock::now();
 
@@ -22,14 +22,14 @@ int main() {
         long long nanoseconds = chrono::duration_cast<chrono::nanoseconds>(duration).count();
 
         cout << "N = " << i << endl;
-        // cout << answer << endl;
+        cout << answer << endl;
         cout << nanoseconds << " nanoseconds." << endl;
     }
 
     for(int i=3; i<=35; ++i) {
         auto start_time = chrono::high_resolution_clock::now();
 
-        // RM6(a,n)
+        long long answer = RM6(3,i);
 
         auto end_time = chrono::high_resolution_clock::now();
 
@@ -38,9 +38,32 @@ int main() {
         long long nanoseconds = chrono::duration_cast<chrono::nanoseconds>(duration).count();
         
         cout << "N = " << i << endl;
-        // cout << answer << endl;
+        cout << answer << endl;
         cout << nanoseconds << " nanoseconds." << endl;
     }
 
     return 0;
+}
+
+
+int RM4(int a, int n) {
+    if (n == 0) {
+        return 1;
+    }
+    else {
+        return a * RM4(a, n - 1);
+    }
+}
+
+
+int RM6(int a, int n) {
+    if (n == 0) {
+        return 1;
+    }
+    else if (n % 2 == 0) {
+        return pow(RM6(a, n/2), 2);
+    }
+    else {
+        return a * pow(RM6(a, n/2), 2);
+    }
 }
